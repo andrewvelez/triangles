437285bc576b83a45138c05f70af887175421474
Andrew Velez <andrewvelez@outlook.com>
2014-06-21 17:53:01 -0400

    satifying domain tests

---
+++ TriangleApi.Tests/Domain/TriangleTests.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+using System;
+
+namespace TriangleApi.Domain
+{
+    public class TriangleTests
+    {
+
+        [Fact]
+        public void Ctor_NegativeParameter_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                delegate
+                {
+                    var badTriangle = new Triangle(-1, 2, 3);
+                });
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                delegate
+                {
+                    var badTriangle = new Triangle(1, -2, 3);
+                });
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                delegate
+                {
+                    var badTriangle = new Triangle(1, 2, -3);
+                });
+        }
+
+        [Fact]
+        public void Ctor_ZeroParameter_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                delegate
+                {
+                    var badTriangle = new Triangle(0, 2, 3);
+                });
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                delegate
+                {
+                    var badTriangle = new Triangle(1, 0, 3);
+                });
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                delegate
+                {
+                    var badTriangle = new Triangle(1, 2, 0);
+                });
+        }
+
+        [Fact]
+        public void Ctor_InfinityParameter_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                delegate
+                {
+                    var badTriangle = new Triangle(Double.PositiveInfinity, 2, 3);
+                });
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                delegate
+                {
+                    var badTriangle = new Triangle(1, Double.PositiveInfinity, 3);
+                });
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                delegate
+                {
+                    var badTriangle = new Triangle(1, 2, Double.PositiveInfinity);
+                });
+        }
+
+    }
+}
---
+++ TriangleApi/Domain/Triangle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace TriangleApi.Domain
+{
+    public class Triangle
+    {
+
+        public enum SideLengthType
+        {
+            Scalene,
+            Isosceles,
+            Equilateral
+        }
+
+        public double Side1 { get; private set; }
+        public double Side2 { get; private set; }
+        public double Side3 { get; private set; }
+
+        public Triangle(double thisSide1, double thisSide2, double thisSide3)
+        {
+            Side1 = GetVerifiedSideLength(thisSide1);
+            Side2 = GetVerifiedSideLength(thisSide2);
+            Side3 = GetVerifiedSideLength(thisSide3);
+        }
+
+        public bool IsEquilateral()
+        {
+            return false;
+        }
+
+        public bool IsIsosceles()
+        {
+            return false;
+        }
+
+        public bool IsScalene()
+        {
+            return false;
+        }
+
+        private SideLengthType GetSideLengthType()
+        {
+            return SideLengthType.Scalene;
+        }
+
+        private double GetVerifiedSideLength(double side)
+        {
+            if (double.IsInfinity(side) || side <= 0.0)
+            {
+                throw new ArgumentOutOfRangeException("For a valid simple triangle, sides should be real positive numbers");
+            }
+            else
+            {
+                return side;
+            }
+        }
+
+    }
+}
